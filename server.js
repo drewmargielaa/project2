833332d36a1ea5be54d387b798cec8c757884210
KunZizinho <kun.zizinho@gmail.com>
2020-06-11 19:47:21 -0400

    working on handlebars

---
+++ server.js
@@ -0,0 +1,37 @@
+//  dependecies
+const express = require("express");
+const handlebars = require("express-handlebars");
+
+var app = express();
+
+var PORT = process.env.PORT || 8000;
+
+// data parsing thru express app
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//  setting up handlebars
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// routes
+app.get("/", function(req, res) {
+    // console.log(req)
+    res.render("index");
+  });
+
+app.get("/job", function(req, res) {
+    // console.log(req)
+    res.render("jobs");
+  });
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+
+
