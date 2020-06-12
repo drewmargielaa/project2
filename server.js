74bec2bf318565b1ebb4677aaea88d41c7d87b1b
Andrew Souza <drewmargielaa@gmail.com>
2020-06-11 21:13:31 -0400

    handle bars

--- server.js
+++ server.js
@@ -1,3 +1,49 @@
+//  dependecies
+const express = require("express");
+const handlebars = require("express-handlebars");
+
+var app = express();
+
+var PORT = process.env.PORT || 8000;
+
+// data parsing thru express app
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//  setting up handlebars
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// routes
+app.get("/", function(req, res) {
+    // console.log(req)
+    res.render("index");
+  });
+
+app.get("/job", function(req, res) {
+    // console.log(req)
+    res.render("jobs");
+  });
+
+  app.get("/learning", function(req, res) {
+    // console.log(req)
+    res.render("learning");
+  });
+
+  app.get("/portfolio", function(req, res) {
+    // console.log(req)
+    res.render("portfolio");
+  });
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
